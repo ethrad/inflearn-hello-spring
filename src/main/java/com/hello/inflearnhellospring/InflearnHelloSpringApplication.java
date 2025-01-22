bb99ed0a59ccb18daec271421e62dbbe372da0fc
ethrad <asdf123f4f4@gmail.com>
2025-01-22 16:04:10 +0900

    init commit

---
+++ src/main/java/com/hello/inflearnhellospring/InflearnHelloSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hello.inflearnhellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnHelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnHelloSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hello/inflearnhellospring/InflearnHelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.inflearnhellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnHelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
